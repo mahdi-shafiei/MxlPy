06a51b1ba14e9546599780e8c8cbc83b02c55b34
Marvin van Aalst <marvin.vanaalst@gmail.com>
2024-11-25 09:25:00 +0100

    dev: chg: fitting routines take pandas objects

--- src/modelbase2/fit.py
+++ src/modelbase2/fit.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import numpy as np
+import pandas as pd
 from scipy.optimize import minimize
 
 from modelbase2.integrators import DefaultIntegrator
@@ -19,86 +20,70 @@ if TYPE_CHECKING:
     from modelbase2.types import ModelProtocol
 
 
-def _steady_state_concs_residual(
+def _steady_state_residual(
     par_values: ArrayLike,
-    data: ArrayLike,
+    data: pd.Series,
     model: ModelProtocol,
     y0: dict[str, float],
     par_names: list[str],
     integrator: type[IntegratorProtocol],
 ) -> float:
-    if (
-        y_ss := Simulator(
+    c_ss, v_ss = (
+        Simulator(
             model.update_parameters(dict(zip(par_names, par_values, strict=True))),
             y0=y0,
             integrator=integrator,
         )
         .simulate_to_steady_state()
-        .get_concs()
-    ) is None:
+        .get_full_concs_and_fluxes()
+    )
+    if c_ss is None or v_ss is None:
         return cast(float, np.inf)
-    return cast(float, np.sqrt(np.mean(np.square(data - y_ss.to_numpy()))))
+    results_ss = pd.concat((c_ss, v_ss), axis=1)
+    diff = data - results_ss.loc[:, data.index]  # type: ignore
+    return cast(float, np.sqrt(np.mean(np.square(diff))))
 
 
-def _steady_state_fluxes_residual(
+def _time_series_residual(
     par_values: ArrayLike,
-    data: ArrayLike,
+    data: pd.DataFrame,
     model: ModelProtocol,
     y0: dict[str, float],
     par_names: list[str],
     integrator: type[IntegratorProtocol],
 ) -> float:
-    if (
-        y_ss := Simulator(
+    c_ss, v_ss = (
+        Simulator(
             model.update_parameters(dict(zip(par_names, par_values, strict=True))),
             y0=y0,
             integrator=integrator,
         )
-        .simulate_to_steady_state()
-        .get_fluxes()
-    ) is None:
+        .simulate(time_points=data.index)  # type: ignore
+        .get_full_concs_and_fluxes()
+    )
+    if c_ss is None or v_ss is None:
         return cast(float, np.inf)
-    return cast(float, np.sqrt(np.mean(np.square(data - y_ss.to_numpy()))))
+    results_ss = pd.concat((c_ss, v_ss), axis=1)
+    diff = data - results_ss.loc[:, data.columns]  # type: ignore
+    return cast(float, np.sqrt(np.mean(np.square(diff))))
 
 
-def _time_series_concs_residual(
-    par_values: ArrayLike,
-    data: ArrayLike,
-    time_points: ArrayLike,
-    model: ModelProtocol,
-    y0: dict[str, float],
-    par_names: list[str],
-    integrator: type[IntegratorProtocol],
-) -> float:
-    if (
-        y := Simulator(
-            model.update_parameters(dict(zip(par_names, par_values, strict=True))),
-            y0=y0,
-            integrator=integrator,
-        )
-        .simulate(time_points=time_points)
-        .get_concs()
-    ) is None:
-        return cast(float, np.inf)
-    return cast(float, np.sqrt(np.mean(np.square(data - y.to_numpy()))))
-
-
-def steady_state_variables(
+def steady_state(
     model: ModelProtocol,
     p0: dict[str, float],
-    y0: dict[str, float],
-    data: ArrayLike,
+    data: pd.Series,
+    y0: dict[str, float] | None = None,
     residual_fn: Callable[
         [
             ArrayLike,
-            ArrayLike,
+            pd.Series,
             ModelProtocol,
             dict[str, float],
             list[str],
             type[IntegratorProtocol],
         ],
         float,
-    ] = _steady_state_concs_residual,
+    ] = _steady_state_residual,
     integrator: type[IntegratorProtocol] = DefaultIntegrator,
 ) -> dict[str, float]:
     par_names = list(p0.keys())
@@ -126,31 +111,27 @@ def steady_state_variables(
     return res
 
 
-def time_series_variables(
+def time_series(
     model: ModelProtocol,
     p0: dict[str, float],
-    y0: dict[str, float],
-    data: ArrayLike,
-    time_points: ArrayLike,
+    data: pd.DataFrame,
+    y0: dict[str, float] | None = None,
     residual_fn: Callable[
         [
             ArrayLike,
-            ArrayLike,
-            ArrayLike,
+            pd.DataFrame,
             ModelProtocol,
             dict[str, float],
             list[str],
             type[IntegratorProtocol],
         ],
         float,
-    ] = _time_series_concs_residual,
+    ] = _time_series_residual,
     integrator: type[IntegratorProtocol] = DefaultIntegrator,
 ) -> dict[str, float]:
     par_names = list(p0.keys())
     x0 = list(p0.values())
     p_orig = model.parameters
-    if len(data) != len(time_points):
-        raise ValueError
 
     res = dict(
         zip(
@@ -158,7 +139,7 @@ def time_series_variables(
             minimize(
                 residual_fn,
                 x0=x0,
-                args=(data, time_points, model, y0, par_names, integrator),
+                args=(data, model, y0, par_names, integrator),
                 bounds=[(1e-12, 1e6) for _ in range(len(p0))],
                 method="L-BFGS-B",
             ).x,
