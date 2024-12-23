5c012b2d71bfcd1b02ea5c6787478ba5d0b761d6
Marvin van Aalst <marvin.vanaalst@gmail.com>
2024-12-23 14:03:47 +0100

    dev: fix: removed file dependency from docs

--- src/modelbase2/parameterise.py
+++ src/modelbase2/parameterise.py
@@ -7,14 +7,16 @@ from parameteriser.brenda.v0 import Brenda
 
 
 def get_km_and_kcat_from_brenda(
-    ec: str, brenda_path: Path
+    ec: str,
+    brenda_path: Path | None = None,
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
     """Obtain michaelis and catalytic constants for given ec number.
 
     You can obtain the database from https://www.brenda-enzymes.org/download.php
     """
     brenda = Brenda()
-    brenda.read_database(brenda_path)
+    if brenda_path is not None:
+        brenda.read_database(brenda_path)
 
     kms, kcats = brenda.get_kms_and_kcats(
         ec=ec,
