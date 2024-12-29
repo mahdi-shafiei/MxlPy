76e82e1880e218d55f67b547f11c3b8068e90ffa
Marvin van Aalst <marvin.vanaalst@gmail.com>
2024-12-29 11:49:45 +0100

    dev: new: experimental codegen

--- src/modelbase2/experimental/__init__.py
+++ src/modelbase2/experimental/__init__.py
@@ -5,6 +5,13 @@ APIs should be considered unstable and may change without notice.
 
 from __future__ import annotations
 
+from .codegen import generate_model_code_py, generate_modelbase_code
+from .diff import model_diff
 from .tex import to_tex
 
-__all__ = ["to_tex"]
+__all__ = [
+    "generate_model_code_py",
+    "generate_modelbase_code",
+    "model_diff",
+    "to_tex",
+]
--- src/modelbase2/experimental/codegen.py
+++ src/modelbase2/experimental/codegen.py
@@ -1,1 +1,210 @@
 """Module to export models as code."""
+
+import ast
+import inspect
+from collections.abc import Callable, Generator, Iterable, Iterator
+
+from modelbase2.model import Model
+from modelbase2.types import Derived
+
+__all__ = [
+    "DocstringRemover",
+    "IdentifierReplacer",
+    "ReturnRemover",
+    "conditional_join",
+    "generate_model_code_py",
+    "generate_modelbase_code",
+    "get_fn_source",
+    "handle_fn",
+]
+
+
+class IdentifierReplacer(ast.NodeTransformer):
+    """Replace identifiers in an AST."""
+
+    def __init__(self, mapping: dict[str, str]) -> None:
+        """Initialize the transformer with a mapping."""
+        self.mapping = mapping
+
+    def visit_Name(self, node: ast.Name) -> ast.Name:  # noqa: N802
+        """Replace the identifier with the mapped value."""
+        return ast.Name(
+            id=self.mapping.get(node.id, node.id),
+            ctx=node.ctx,
+        )
+
+
+class DocstringRemover(ast.NodeTransformer):
+    """Remove docstrings from an AST."""
+
+    def visit_Expr(self, node: ast.Expr) -> ast.Expr | None:  # noqa: N802
+        """Remove docstrings."""
+        if isinstance(const := node.value, ast.Constant) and isinstance(
+            const.value, str
+        ):
+            return None
+        return node
+
+
+class ReturnRemover(ast.NodeTransformer):
+    """Remove return statements from an AST."""
+
+    def visit_Return(self, node: ast.Return) -> ast.expr | None:  # noqa: N802
+        """Remove return statements."""
+        return node.value
+
+
+def get_fn_source(fn: Callable) -> ast.FunctionDef:
+    """Get the source code of a function as an AST."""
+    import inspect
+    import textwrap
+
+    import dill
+
+    try:
+        source = inspect.getsource(fn)
+    except OSError:  # could not get source code
+        source = dill.source.getsource(fn)
+
+    tree = ast.parse(textwrap.dedent(source))
+    if not isinstance(fn_def := tree.body[0], ast.FunctionDef):
+        msg = "Not a function"
+        raise TypeError(msg)
+    return fn_def
+
+
+def handle_fn(fn: Callable, args: list[str]) -> str:
+    """Get the source code of a function, removing docstrings and return statements."""
+    tree = get_fn_source(fn)
+
+    argmap = dict(zip([i.arg for i in tree.args.args], args, strict=True))
+    tree = DocstringRemover().visit(tree)
+    tree = IdentifierReplacer(argmap).visit(tree)
+    tree = ReturnRemover().visit(tree)
+    return ast.unparse(tree.body)
+
+
+def conditional_join[T](
+    iterable: Iterable[T],
+    question: Callable[[T], bool],
+    true_pat: str,
+    false_pat: str,
+) -> str:
+    """Join an iterable, applying a pattern to each element based on a condition."""
+
+    def inner(it: Iterator[T]) -> Generator[str, None, None]:
+        yield str(next(it))
+        while True:
+            try:
+                el = next(it)
+                if question(el):
+                    yield f"{true_pat}{el}"
+                else:
+                    yield f"{false_pat}{el}"
+            except StopIteration:
+                break
+
+    return "".join(inner(iter(iterable)))
+
+
+def generate_modelbase_code(model: Model) -> str:
+    """Generate a modelbase model from a model."""
+    variables = model.variables
+    parameters = model.parameters
+    functions = {}
+
+    derived_source = []
+    for k, v in model.derived.items():
+        fn = v.fn
+        fn_name = fn.__name__
+        functions[fn_name] = inspect.getsource(fn)
+
+        derived_source.append(
+            f""".add_derived(
+                "{k}",
+                fn={fn_name},
+                args={v.args},
+            )"""
+        )
+
+    reactions_source = []
+    for k, v in model.reactions.items():
+        fn = v.fn
+        fn_name = fn.__name__
+        functions[fn_name] = inspect.getsource(fn)
+        reactions_source.append(
+            f"""        .add_reaction(
+                "{k}",
+                fn={fn_name},
+                args={v.args},
+                stoichiometry={v.stoichiometry},
+            )"""
+        )
+
+    functions_source = "\n".join(functions.values())
+
+    source = [
+        "from modelbase2 import Model\n",
+        functions_source,
+        "def create_model() -> Model:",
+        "    return (",
+        "        Model()",
+    ]
+
+    if len(parameters) > 0:
+        source.append(f"        .add_parameters({parameters})")
+
+    if len(variables) > 0:
+        source.append(f"        .add_variables({variables})")
+
+    if len(derived_source) > 0:
+        source.append("\n".join(derived_source))
+
+    if len(reactions_source) > 0:
+        source.append("\n".join(reactions_source))
+
+    source.append("    )")
+
+    return "\n".join(source)
+
+
+def generate_model_code_py(model: Model) -> str:
+    """Transform the model into a single function, inlining the function calls."""
+    variables = model.variables
+    variable_source = ", ".join(f"{k}: Float" for k in variables)
+
+    parameter_source = "\n".join(f"    {k} = {v}" for k, v in model.parameters.items())
+    reactions = []
+    for name, rxn in model.reactions.items():
+        reactions.append(f"    {name} = {handle_fn(rxn.fn, rxn.args)}")
+
+    stoichiometries = {}
+    for rxn_name, rxn in model._reactions.items():  # noqa: SLF001
+        for cpd_name, factor in rxn.stoichiometry.items():
+            if isinstance(factor, Derived):
+                src = f"{handle_fn(factor.fn, factor.args)} * {rxn_name}"
+            elif factor == 1:
+                src = rxn_name
+            elif factor == -1:
+                src = f"- {rxn_name}"
+            else:
+                src = f"{factor} * {rxn_name}"
+            stoichiometries.setdefault(cpd_name, []).append(src)
+
+    stoich_source = []
+    for variable, stoich in stoichiometries.items():
+        stoich_source.append(
+            f"    d{variable}dt = {conditional_join(stoich, lambda x: x.startswith("-"), " ", " + ")}"
+        )
+
+    source = [
+        f"def model({variable_source}) -> Float:",
+        parameter_source,
+        *reactions,
+        *stoich_source,
+        "    return {}".format(
+            ", ".join(f"d{i}dt" for i in variables),
+        ),
+    ]
+
+    return "\n".join(source)
