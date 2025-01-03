9c2483fa11dcbb7a6cf29015bb39f5edfcc4f570
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-01-03 11:06:03 +0100

    dev: new: polynomial surrogates

--- src/modelbase2/surrogates/__init__.py
+++ src/modelbase2/surrogates/__init__.py
@@ -21,8 +21,12 @@ import contextlib
 with contextlib.suppress(ImportError):
     from ._torch import Dense, TorchSurrogate, train_torch_surrogate
 
+from ._poly import PolySurrogate, train_polynomial_surrogate
+
 __all__ = [
     "Dense",
+    "PolySurrogate",
     "TorchSurrogate",
+    "train_polynomial_surrogate",
     "train_torch_surrogate",
 ]
