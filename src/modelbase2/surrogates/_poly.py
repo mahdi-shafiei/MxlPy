a8f41127497fb7a51a225e5ff8a73e792c4ad302
Tim Nies <t.nies93@gmail.com>
2025-01-22 13:11:02 +0100

    dev: new: add series option in surrogates

--- src/modelbase2/surrogates/_poly.py
+++ src/modelbase2/surrogates/_poly.py
@@ -3,29 +3,46 @@ from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
-from numpy.polynomial.polynomial import Polynomial
+from numpy import polynomial
+from typing import Union
 
 from modelbase2.types import AbstractSurrogate, ArrayLike
 
-__all__ = ["PolySurrogate", "train_polynomial_surrogate"]
+__all__ = [
+    "PolySurrogate",
+    "PolynomialType",
+    "SeriesExpansion",
+    "train_series_surrogate",
+]
+
+# define custom type #TODO think about
+SeriesExpansion = PolynomialType = (
+    polynomial.polynomial.Polynomial
+    | polynomial.chebyshev.Chebyshev
+    | polynomial.legendre.Legendre
+    | polynomial.laguerre.Laguerre
+    | polynomial.hermite.Hermite
+    | polynomial.hermite_e.HermiteE
+)
 
 
 @dataclass(kw_only=True)
 class PolySurrogate(AbstractSurrogate):
-    model: Polynomial
+    model: SeriesExpansion
 
     def predict_raw(self, y: np.ndarray) -> np.ndarray:
         return self.model(y)
 
 
-def train_polynomial_surrogate(
+def train_series_surrogate(
     feature: ArrayLike,
     target: ArrayLike,
+    series: str = "Power",
     degrees: Iterable[int] = (1, 2, 3, 4, 5, 6, 7),
     surrogate_args: list[str] | None = None,
     surrogate_stoichiometries: dict[str, dict[str, float]] | None = None,
 ) -> tuple[PolySurrogate, pd.DataFrame]:
-    """Train a polynomial surrogate model.
+    """Train a surrogate model based on function series expansion.
 
     Args:
         feature: Input data as a numpy array.
@@ -41,7 +58,19 @@ def train_polynomial_surrogate(
     feature = np.array(feature, dtype=float)
     target = np.array(target, dtype=float)
 
-    models = [Polynomial.fit(feature, target, degree) for degree in degrees]
+    # Choose numpy polynomial convenience classes
+    series_dictionary = {
+        "Power": polynomial.polynomial.Polynomial,
+        "Chebyshev": polynomial.chebyshev.Chebyshev,
+        "Legendre": polynomial.legendre.Legendre,
+        "Laguerre": polynomial.laguerre.Laguerre,
+        "Hermite": polynomial.hermite.Hermite,
+        "HermiteE": polynomial.hermite_e.HermiteE,
+    }
+
+    fn_series = series_dictionary[series]
+
+    models = [fn_series.fit(feature, target, degree) for degree in degrees]
     predictions = np.array([model(feature) for model in models], dtype=float)
     errors = np.sqrt(np.mean(np.square(predictions - target.reshape(1, -1)), axis=1))
     log_likelihood = -0.5 * np.sum(
