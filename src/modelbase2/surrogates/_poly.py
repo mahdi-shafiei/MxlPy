e06ec188e3bbe3e09cc7be28129975c7d4917fc1
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-04-07 07:04:56 +0200

    dev: fix: literal import

--- src/modelbase2/surrogates/_poly.py
+++ src/modelbase2/surrogates/_poly.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 from numpy import polynomial
-from typing import Union
+from typing import Union, Literal
 
 from modelbase2.types import AbstractSurrogate, ArrayLike
 
