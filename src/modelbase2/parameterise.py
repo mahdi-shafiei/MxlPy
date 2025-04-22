655d138dfe7e0fd1544de197c68cc096668e60a1
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-04-22 15:13:22 +0200

    dev: new: tests for parameterise module

--- src/modelbase2/parameterise.py
+++ src/modelbase2/parameterise.py
@@ -17,8 +17,7 @@ def get_km_and_kcat_from_brenda(
     You can obtain the database from https://www.brenda-enzymes.org/download.php
     """
     brenda = Brenda()
-    if brenda_path is not None:
-        brenda.read_database(brenda_path)
+    brenda.read_database(brenda_path)
 
     kms, kcats = brenda.get_kms_and_kcats(
         ec=ec,
