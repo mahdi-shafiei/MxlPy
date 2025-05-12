293129acc2ae3b33f25b7dcab831abf941ba1699
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-05-12 10:43:51 +0200

    usr: new: fit protocol time course

--- src/mxlpy/fit.py
+++ src/mxlpy/fit.py
@@ -30,12 +30,14 @@ __all__ = [
     "InitialGuess",
     "LossFn",
     "MinimizeFn",
+    "ProtocolResidualFn",
     "ResidualFn",
     "SteadyStateResidualFn",
     "TimeSeriesResidualFn",
     "rmse",
     "steady_state",
     "time_course",
+    "time_course_over_protocol",
 ]
 
 if TYPE_CHECKING:
@@ -94,6 +96,26 @@ class TimeSeriesResidualFn(Protocol):
         y0: dict[str, float],
         integrator: IntegratorType,
         loss_fn: LossFn,
+    ) -> float:
+        """Calculate residual error between model time course and experimental data."""
+        ...
+
+
+class ProtocolResidualFn(Protocol):
+    """Protocol for time series residual functions."""
+
+    def __call__(
+        self,
+        par_values: Array,
+        # This will be filled out by partial
+        par_names: list[str],
+        data: pd.DataFrame,
+        model: Model,
+        y0: dict[str, float],
+        integrator: IntegratorType,
+        loss_fn: LossFn,
+        protocol: pd.DataFrame,
+        time_points_per_step: int = 10,
     ) -> float:
         """Calculate residual error between model time course and experimental data."""
         ...
@@ -215,6 +237,57 @@ def _time_course_residual(
     )
 
 
+def _protocol_residual(
+    par_values: ArrayLike,
+    # This will be filled out by partial
+    par_names: list[str],
+    data: pd.DataFrame,
+    model: Model,
+    y0: dict[str, float] | None,
+    integrator: IntegratorType,
+    loss_fn: LossFn,
+    protocol: pd.DataFrame,
+    time_points_per_step: int = 10,
+) -> float:
+    """Calculate residual error between model time course and experimental data.
+
+    Args:
+        par_values: Parameter values to test
+        data: Experimental time course data
+        model: Model instance to simulate
+        y0: Initial conditions
+        par_names: Names of parameters being fit
+        integrator: ODE integrator class to use
+        loss_fn: Loss function to use for residual calculation
+        protocol: Experimental protocol
+        time_points_per_step: Number of time points per step in the protocol
+
+    Returns:
+        float: Root mean square error between model and data
+
+    """
+    res = (
+        Simulator(
+            model.update_parameters(dict(zip(par_names, par_values, strict=True))),
+            y0=y0,
+            integrator=integrator,
+        )
+        .simulate_over_protocol(
+            protocol=protocol,
+            time_points_per_step=time_points_per_step,
+        )
+        .get_result()
+    )
+    if res is None:
+        return cast(float, np.inf)
+    results_ss = res.get_combined()
+
+    return loss_fn(
+        results_ss.loc[:, cast(list, data.columns)],
+        data,
+    )
+
+
 def steady_state(
     model: Model,
     p0: dict[str, float],
@@ -290,7 +363,7 @@ def time_course(
 
     Args:
         model: Model instance to fit
-        data: Experimental time course data as pandas DataFrame
+        data: Experimental time course data
         p0: Initial parameter guesses as {parameter_name: value}
         y0: Initial conditions as {species_name: value}
         minimize_fn: Function to minimize fitting error
@@ -318,6 +391,65 @@ def time_course(
             par_names=par_names,
             integrator=integrator,
             loss_fn=loss_fn,
+        ),
+    )
+    res = minimize_fn(fn, p0)
+    model.update_parameters(p_orig)
+    return res
+
+
+def time_course_over_protocol(
+    model: Model,
+    p0: dict[str, float],
+    data: pd.DataFrame,
+    protocol: pd.DataFrame,
+    y0: dict[str, float] | None = None,
+    minimize_fn: MinimizeFn = _default_minimize_fn,
+    residual_fn: ProtocolResidualFn = _protocol_residual,
+    integrator: IntegratorType = DefaultIntegrator,
+    loss_fn: LossFn = rmse,
+    time_points_per_step: int = 10,
+) -> dict[str, float]:
+    """Fit model parameters to time course of experimental data.
+
+    Examples:
+        >>> time_course(model, p0, data)
+        {'k1': 0.1, 'k2': 0.2}
+
+    Args:
+        model: Model instance to fit
+        p0: Initial parameter guesses as {parameter_name: value}
+        data: Experimental time course data
+        protocol: Experimental protocol
+        y0: Initial conditions as {species_name: value}
+        minimize_fn: Function to minimize fitting error
+        residual_fn: Function to calculate fitting error
+        integrator: ODE integrator class
+        loss_fn: Loss function to use for residual calculation
+        time_points_per_step: Number of time points per step in the protocol
+
+    Returns:
+        dict[str, float]: Fitted parameters as {parameter_name: fitted_value}
+
+    Note:
+        Uses L-BFGS-B optimization with bounds [1e-12, 1e6] for all parameters
+
+    """
+    par_names = list(p0.keys())
+    p_orig = model.parameters
+
+    fn = cast(
+        ResidualFn,
+        partial(
+            residual_fn,
+            data=data,
+            model=model,
+            y0=y0,
+            par_names=par_names,
+            integrator=integrator,
+            loss_fn=loss_fn,
+            protocol=protocol,
+            time_points_per_step=time_points_per_step,
         ),
     )
     res = minimize_fn(fn, p0)
